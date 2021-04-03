3350ee7c2e14c729a61219092f8ac87bd1003722
P3PP3R4 <81479058+P3PP3R4@users.noreply.github.com>
2021-04-03 22:55:17 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,52 @@
+/*
+function LinksSetColor(color) {
+    var list = document.querySelectorAll('a');
+    var i = 0;
+    while(i < list.length) {
+        console.log(list[i]);
+        list[i].style.color = color;
+        i += 1;
+    }
+}*/
+
+var Links = {
+    setColor:function(color) {
+        // var list = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < list.length) {
+        //     console.log(list[i]);
+        //     list[i].style.color = color;
+        //     i += 1;
+        // }
+        $('a').css('color', color);
+    }
+}
+var Body = {
+    setColor:function(color) {
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+    },
+    setBackgroundColor:function(color) {
+        // document.querySelector('body').style.backgroundColor = color;\
+        $('body').css('backgroundColor', color);
+    }
+}
+
+function nightDayHandler(self) {
+    var target = document.querySelector('body');
+
+    if(self.value === 'night') {
+        Body.setColor('white');
+        Body.setBackgroundColor('black');
+        document.querySelector('#toggle').value = 'day';
+
+        Links.setColor('powderblue');
+    }
+    else {
+        Body.setColor('black');
+        Body.setBackgroundColor('white');
+        self.value = 'night';
+        
+        Links.setColor('blue');
+    }
+}
